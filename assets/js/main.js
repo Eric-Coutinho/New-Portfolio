c27177f3ffed47cbc4c8dfa03d5174f364246a1a
eric-coutinho <eric.coutinhop@hotmail.com>
2024-06-28 22:31:40 -0300

    Showing projects done

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,11 +4,38 @@ function fetchRepos() {
   fetch(`https://api.github.com/users/${username}/repos`)
     .then((response) => response.json())
     .then((data) => {
-      data.forEach((repo) => {
-        console.log(repo);
-      });
+      localStorage.setItem("repos", JSON.stringify(data));
+      renderRepos();
     })
     .catch((error) => console.error("Erro:", error));
+}
+
+function renderRepos() {
+  const repos = JSON.parse(localStorage.getItem("repos"));
+  const projectsContainer = document.getElementById("projects");
+  projectsContainer.innerHTML = "";
+
+  repos.forEach((repo) => {
+    const rolDiv = document.createElement("div");
+    rolDiv.className = "col-12 col-sm-8 col-md-4";
+    rolDiv.innerHTML = `
+          <div class="card cardRepos">
+            <div class="card-body cardBodyRepo">
+              <div class="card-title">${repo.name}</div>
+              <div class="card-text description">${
+                repo.description || "Sem descrição"
+              }</div>
+              <div class="card-text center">${
+                "Linguagem: " + (repo.language || "Indefinida")
+              }</div>
+              <div class="card-footer"><a href="${
+                repo.html_url
+              }" target="_blank">Link para o projeto</a></div>
+            </div>
+          </div>
+        `;
+    projectsContainer.appendChild(rolDiv);
+  });
 }
 
 fetchRepos();
